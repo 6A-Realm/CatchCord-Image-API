3dbd78b78c49c0c12e46191176f197cef76ff707
6A <78461864+6A-Realm@users.noreply.github.com>
2022-04-08 11:34:49 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -11,11 +11,11 @@ App.get('/home', async (DataRequested, DataResponse) => {
 })
 
 // API Image Handling
-App.post('/main/:dex/:type', async (DataRequested, DataResponse) => {
+App.post('/main', async (DataRequested, DataResponse) => {
         
     // Check if values are assigned correctly
-    const { dex } = DataRequested.params;
-    const { type } = DataRequested.params;
+    const { dex } = DataRequested.body;
+    const { type } = DataRequested.body;
 
     if (!dex) {
         DataResponse.status(400)
